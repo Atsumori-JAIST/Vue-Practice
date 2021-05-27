631b9dd8210f4254ff722c1f0c2c0b4e498662bc
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-28 08:54:31 +0900

    Backup 05/28-08:54

--- todo/app.js
+++ todo/app.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Example app listening on port 8000!');
 });
