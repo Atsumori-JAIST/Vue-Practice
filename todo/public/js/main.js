45b2576efc4a00cd0bb2dc30af8fccea3723c2ad
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-28 20:18:16 +0900

    05/28-20:18

--- todo/public/js/main.js
+++ todo/public/js/main.js
@@ -3,17 +3,31 @@ const app = new Vue({
     vuetify: new Vuetify(),
 
     data: {
-        selectedState: 0,
+        selectedState: -1,
         status: [
             { value: -1, label: 'すべて' },
             { value:  0, label: '作業中' },
-            { value:  1, label: '完了'   }
+            { value:  1, label: '完了'   },
         ],
-        selectedItem: 1,
-        items: [
-            { text: 'Real-Time', icon: 'mdi-clock' },
-            { text: 'Audience', icon: 'mdi-account' },
-            { text: 'Conversions', icon: 'mdi-flag' },
+        headers: [
+            { text: 'Task',  value: 'task' },
+            { text: 'Progress', value: 'progress' },
+        ],
+        todos: [
+            { progress: 0, task: '買い物' },
+            { progress: 1, task: 'アルゴリズム課題' },
+            { progress: 0, task: '筋トレ' },
         ]
-    }
+    },
+
+    computed: {
+    },
+
+    methods: {
+        getColor(progress) {
+            if (progress == 0) return 'light-blue darken-1';
+            else if (progress == 1) return 'red darken-4';
+            else return 'blue-grey darken-3';
+        }
+    },
 })
