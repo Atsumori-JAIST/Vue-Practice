bf4ae864a3d3c7ff5c73c4ad4461d19a8c049a93
Atsumori-JAIST <s2010401@jaist.ac.jp>
2021-05-28 17:00:53 +0900

    Backup 05/28-17:00

--- todo/public/js/main.js
+++ todo/public/js/main.js
@@ -1,1 +1,13 @@
-console.log('ok!')
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+
+    data: {
+        selectedItem: 1,
+        items: [
+            { text: 'Real-Time', icon: 'mdi-clock' },
+            { text: 'Audience', icon: 'mdi-account' },
+            { text: 'Conversions', icon: 'mdi-flag' },
+        ]
+    }
+})
